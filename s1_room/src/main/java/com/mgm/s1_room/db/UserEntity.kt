ce0d3b8d409e48af735c8dbcc217ba22bd379655
Golmoradi Majid (Electronic Banking Services) <M.Golmoradi@mebank.com>
2022-07-12 09:40:07 +0430

    Part 4-room entity

---
+++ s1_room/src/main/java/com/mgm/s1_room/db/UserEntity.kt
@@ -0,0 +1,20 @@
+package com.mgm.s1_room.db
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.mgm.s1_room.utils.Constants
+
+/**
+ * Created by Majid-Golmoradi on 7/12/2022.
+ * Email: golmoradi.majid@gmail.com
+ */
+
+@Entity(tableName = Constants.USER_TABLE)
+data class UserEntity(
+    @PrimaryKey(autoGenerate = true)
+    val userId:Int,
+    @ColumnInfo(name = "user_name")
+    val userName:String,
+    val userAge:Int
+)
---
+++ s1_room/src/main/java/com/mgm/s1_room/utils/Constants.kt
@@ -0,0 +1,9 @@
+package com.mgm.s1_room.utils
+
+/**
+ * Created by Majid-Golmoradi on 7/12/2022.
+ * Email: golmoradi.majid@gmail.com
+ */
+object Constants {
+    const val USER_TABLE = "user_table"
+}
