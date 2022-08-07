30c5776f7df76d36e7b67edaf212f67790f29d11
Majid Golmoradi <golmoradi.majid@gmail.com>
2022-08-07 14:59:32 +0430

    part 21

--- s1-coroutine/src/main/java/com/mgm/s1_coroutine/MainActivity.kt
+++ s1-coroutine/src/main/java/com/mgm/s1_coroutine/MainActivity.kt
@@ -2,10 +2,32 @@ package com.mgm.s1_coroutine
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.mgm.s1_coroutine.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    //Binding
+    private lateinit var binding:ActivityMainBinding
+
+    //Other
+    private val TAG = "coroutineTag"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+
+        CoroutineScope(Dispatchers.IO).launch {
+            shoMyLog()
+        }
+    }
+
+    private suspend fun shoMyLog(){
+        delay(3000)
+        Log.e(TAG, Thread.currentThread().name)
     }
 }
