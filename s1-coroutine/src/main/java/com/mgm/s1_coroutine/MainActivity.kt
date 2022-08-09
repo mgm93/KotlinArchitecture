1ee35e7825a5d3e177fa51b19fb744ae3229bbb3
Majid Golmoradi <golmoradi.majid@gmail.com>
2022-08-09 15:24:44 +0430

    part 26 - runBlocking, custom thread,change ui by io thread

--- s1-coroutine/src/main/java/com/mgm/s1_coroutine/MainActivity.kt
+++ s1-coroutine/src/main/java/com/mgm/s1_coroutine/MainActivity.kt
@@ -18,29 +18,29 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
         /*CoroutineScope(Dispatchers.IO).launch {
             shoMyLog()
         }*/
 
-        CoroutineScope(Dispatchers.IO).launch {
-            /**
+/*        CoroutineScope(Dispatchers.Main).launch {
+            *//**
              * do tasks synchronized
-             */
-/*
+             *//*
+*//*
             val time  = measureTimeMillis {
                 val result1 = task1()
                 val result2 = task2()
                 Log.e(TAG, result1)
                 Log.e(TAG, result2)
             }
-            Log.e(TAG, time.toString())*/
+            Log.e(TAG, time.toString())*//*
 
-            /**
+            *//**
              * do tasks Asynchronized
              *
-             */
+             *//*
             val result1 = async { task1() }
             val result2 = async { task2() }
             var finalResult = ""
@@ -50,7 +50,52 @@ class MainActivity : AppCompatActivity() {
             Log.e(TAG, "$finalResult - ${time.toString()}")
 
 
+        }*/
+
+        /**
+         * runBlocking
+         */
+        /*CoroutineScope(Dispatchers.Main).launch {
+            doWork1()
+            doWork1()
+            doWork1()
         }
+
+        CoroutineScope(Dispatchers.Main).launch {
+            runBlocking {
+                doWork2()
+                delay(3000)
+                doWork2()
+                doWork2()
+            }
+        }*/
+
+        /**
+         * Custom thread name
+         */
+        /*CoroutineScope(newSingleThreadContext("Majid")).launch {
+            Log.e(TAG, Thread.currentThread().name)
+        }*/
+
+        /**
+         * change ui on IO Thread
+         */
+        CoroutineScope(Dispatchers.IO).launch {
+            doWork1()
+            withContext(Dispatchers.Main){
+                binding.infoTxt.text = "Done"
+            }
+        }
+    }
+
+    private suspend fun doWork1(){
+        delay(1000)
+        Log.e(TAG, "work1")
+    }
+
+    private suspend fun doWork2(){
+        delay(1000)
+        Log.e(TAG,"work2")
     }
 
     private suspend fun task1():String{
