25cfcb4553d748298f5088696a826c03d0141507
Golmoradi Majid (Electronic Banking Services) <M.Golmoradi@mebank.com>
2022-08-22 15:27:14 +0430

    Part 41 - Hilt & retrofit

--- s1-hilt/src/main/java/com/mgm/s1_hilt/Constants.kt
+++ s1-hilt/src/main/java/com/mgm/s1_hilt/Constants.kt
@@ -11,3 +11,8 @@ const val NAMED_APP_INFO = "named_app_info"
 
 const val NOTE_TABLE = "note_table"
 const val NOTE_DATABASE = "note_database"
+
+const val BASE_URL = "https://moviesapi.ir/api/v1/"
+const val NETWORK_TIMEOUT = 60L
+const val NAMED_HEADER = "named_header"
+const val NAMED_BODY = "named_body"
